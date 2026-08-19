32ee0cf38b37491a362c4622a884afbb41861968
Siri15077 <etikala09@gmail.com>
2026-08-19 20:57:16 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -2,9 +2,13 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-MODEL_PATH = "model/best_waste_model.h5"
+MODEL_PATH = "model/waste_model.tflite"
 
-model = tf.keras.models.load_model(MODEL_PATH)
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 class_names = [
     "cardboard",
@@ -19,19 +23,28 @@ class_names = [
 def predict_waste(image_path):
 
     image = Image.open(image_path).convert("RGB")
-
     image = image.resize((160, 160))
 
-    image_array = np.array(image) / 255.0
-
+    image_array = np.array(image, dtype=np.float32) / 255.0
     image_array = np.expand_dims(image_array, axis=0)
 
-    predictions = model.predict(image_array, verbose=0)
+    interpreter.set_tensor(
+        input_details[0]["index"],
+        image_array
+    )
+
+    interpreter.invoke()
+
+    predictions = interpreter.get_tensor(
+        output_details[0]["index"]
+    )
 
     predicted_index = np.argmax(predictions[0])
 
     predicted_class = class_names[predicted_index]
 
-    confidence = predictions[0][predicted_index] * 100
+    confidence = float(
+        predictions[0][predicted_index] * 100
+    )
 
     return predicted_class, confidence
